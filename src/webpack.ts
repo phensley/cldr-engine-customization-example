2e5404b08bf153599ce4db51201c31adfa6f960d
Patrick Hensley <pathensley@gmail.com>
2019-04-10 17:40:23 -0400

    Statically bundle more languages

--- src/webpack.ts
+++ src/webpack.ts
@@ -4,10 +4,22 @@ import * as config from './config.json';
 // statically embed languages
 import * as english from '../packs/en.json';
 import * as spanish from '../packs/es.json';
+import * as french from '../packs/fr.json';
+import * as italian from '../packs/it.json';
+import * as german from '../packs/de.json';
+import * as japanese from '../packs/ja.json';
+import * as korean from '../packs/ko.json';
+import * as chinese from '../packs/zh.json';
 
 const packs = {
-  'en': english,
-  'es': spanish
+  en: english,
+  es: spanish,
+  fr: french,
+  it: italian,
+  de: german,
+  ja: japanese,
+  ko: korean,
+  zh: chinese
 };
 
 const loader = (language: string): any => packs[language];
